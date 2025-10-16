5a68e068d5184169f27c231a99f65e49c52043f6
Yaroslav Anikovich <y.anikovich@21vek.by>
2025-10-16 04:35:13 +0400

    Added logger handlers

--- utils.py
+++ utils.py
@@ -1,8 +1,13 @@
 import requests
 from time import sleep
 import logging
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("uvicorn")
 logger.setLevel(logging.DEBUG)
+if not logger.hasHandlers():
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
 
 
